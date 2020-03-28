4e963b395f3c8705cd73debde6c1173a8336d600
Greg White <grewhit25@gmail.com>
2020-03-28 17:12:44 +0000

    3tl password

--- multi-arch-docker-ci.sh
+++ multi-arch-docker-ci.sh
@@ -66,7 +66,8 @@ function multi_arch_docker::install_docker_buildx() {
 #   DOCKER_USERNAME ... user name of Docker Hub account
 #   DOCKER_PASSWORD ... password of Docker Hub account
 function multi_arch_docker::login_to_docker_hub() {
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 }
 
 # Run buildx build and push.
